6649594e76841ac0b4e813157d303bb27a11aa23
Fernando Penaranda <fernandopenaranda@github.com>
2025-12-27 19:43:02 +0100

    fixpaths

--- src/cluster/cluster_tools.jl
+++ src/cluster/cluster_tools.jl
@@ -6,10 +6,11 @@ function script_path(name::String)
     return path
 end
 
-function slurm_submit_interpolations(;Ezmin = -6, Ezmax = 6, evals = 10, N = 7, eta = 0.05,  
-    dryrun=false)
-    lmcfolder = dirname(pathof(LMC))
+function slurm_submit_interpolations(;Ezmin = -6, Ezmax = 6, 
+    evals = 10, N = 7, eta = 0.05,  dryrun=false)
+    lmcfolder = dirname(pathof(LMC)) * "/cluster/interpolations.jl"
     script = script_path("run_interpolations.sh")
+    println("bash_file_name: ", script)
     cmd = `sbatch $script $lmcfolder $Ezmin $Ezmax $evals $N $eta`
     dryrun && return cmd
     run(cmd)
