f2d11056c4dfbeef04ae60ca32102d5d6e6593ee
Fernando Penaranda <fernandopenaranda@github.com>
2025-12-29 16:49:41 +0100

    slurm path

--- src/cluster/bash/run_phasediagrams.sh
+++ src/cluster/bash/run_phasediagrams.sh
@@ -30,4 +30,4 @@ echo "My SLURM_ARRAY_TASK_ID is $SLURM_ARRAY_TASK_ID"
 echo "Array length: $SLURM_ARRAY_TASK_MAX"
 echo "pathoffile: $pathtofile"
 
-/scratch/ferpe/.juliaup/bin/julia --compiled-modules=no $pathtofile $SLURM_ARRAY_TASK_ID $SLURM_ARRAY_TASK_MAX $SLURM_ARRAY_JOB_ID $interpolPID $Ezsteps $nu_min $nu_max $nu_points $U $J $lambda $eta $estimated_bound_width $iterations $int_mode $random_guesses
+/scratch/ferpe/julia-1.9.4/bin/julia --compiled-modules=no $pathtofile $SLURM_ARRAY_TASK_ID $SLURM_ARRAY_TASK_MAX $SLURM_ARRAY_JOB_ID $interpolPID $Ezsteps $nu_min $nu_max $nu_points $U $J $lambda $eta $estimated_bound_width $iterations $int_mode $random_guesses
