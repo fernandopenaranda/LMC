2cb306e497335053bd7f2ac8448cee376ba55986
Fernando Penaranda <fernandopenaranda@github.com>
2025-12-30 01:14:35 +0100

    more nodes

--- src/cluster/bash/run_observable.sh
+++ src/cluster/bash/run_observable.sh
@@ -6,7 +6,7 @@
 #SBATCH --cpus-per-task=1        # cpu-cores per task (>1 ifmulti-threaded tasks)
 #SBATCH --mem-per-cpu=8G         # memory per cpu-core (4G is default)
 #SBATCH --time=23:00:00          # total run time limit (HH:MM:SS)
-#SBATCH --array=0-99             # job array with index values 0, 1, 2, 3, 4
+#SBATCH --array=0-299             # job array with index values 0, 1, 2, 3, 4
 #SBATCH --error=slurm-%A.%a.err
 #SBATCH --mail-user=fernando.penaranda@dipc.org
 
