81f5536276c9ff713b195e7bea772bcfba9519a2
Fernando Penaranda <fernandopenaranda@github.com>
2025-12-31 20:00:03 +0100

    add merged data procedure

--- src/LMC.jl
+++ src/LMC.jl
@@ -45,6 +45,7 @@ module LMC
     include(rhomb_folder * "observable_maps.jl")
     include(common_folder * "separate_contributions.jl")
     include(proj_folder * "/cluster/cluster_tools.jl")
+    include(proj_folder * "/cluster/merged_data.jl")
 
     
 
@@ -65,5 +66,5 @@ module LMC
     export klmc, kresolved_Ωz, kresolved_Ωin, kresolved_dOMM, evalmat, plotmap, plotmap!
     export save_to_csv
     export lmc_map, drude_map, ahe_map, map_eval, Ez_map_eval
-    export script_path, slurm_submit_interpolations, slurm_submit_phasediagrams, slurm_submit_observable
+    export script_path, slurm_submit_interpolations, slurm_submit_phasediagrams, slurm_submit_observable, data_merge
 end
---
+++ src/cluster/merged_data.jl
@@ -0,0 +1,30 @@
+#using LMC, JLD2 # needs to load LMC structures to approately read the preset files
+""" once the slurm calculation if finished we run these julia functions to create merged data files"""
+function data_merge(folderPID)
+    merged = Dict{String,Any}()
+    pathtoPID = find_folder(folderPID) # run in LMC folder project creates the absolute path to PID
+    pathtofiles = glob(joinpath("*", "data.jld"), pathtoPID) 
+    sorted_paths = sort( pathtofiles, by = p -> parse(Int, basename(dirname(p)))) # sort numerically by folder order which is Ez order
+    pathtopresets = glob(joinpath("*", "presets.jld"), pathtoPID) 
+    for f in sorted_paths
+        merged[f] = load(f)
+    end
+    presets = load(pathtopresets[1])
+    @save pathtoPID * "/merged_pd_data.jld" merged # data
+    @save pathtoPID * "/merged_presets" presets
+end
+
+""" PID folder finder: e.g. PID = 0000001, 
+    `find_folder(PID)`
+ find the absolute path to PID folder regardless the data
+ store phase diagrams, drude, qah, LMCS responses... """
+find_folder(PID) = find_folder(string(PID))
+function find_folder(target::AbstractString)
+    root = pwd()
+    for (dirpath, dirnames, _) in walkdir(root)
+        if target in dirnames
+            return joinpath(dirpath, target)
+        end
+    end
+    return nothing
+end
